47b2f6a9475f31dd5fb6326b8f939684d7846890
Amir Akhavan <amir.akhavans@gmail.com>
2022-04-29 14:06:28 +0300

    Asset Added

--- src/release.config.js
+++ src/release.config.js
@@ -2,10 +2,16 @@ module.exports = {
     branches: "master",
     repositoryUrl: "https://github.com/amir-akhavans/react-app-project",
     plugins: [
-        '@semantic-release/commit-analyzer', // Analyses commit messages and decides what the next version will be based on the commit messages
+        '@semantic-release/commit-analyzer',           // Analyses commit messages and decides what the next version will be based on the commit messages
         '@semantic-release/release-notes-generator',   // Generates release notes based on commit messages descriptions
-    //  '@semantic-release/npm',       //used if you are releasing an npm package
-        '@semantic-release/github'   // uses for creating a Github release
-    ],
+    //  '@semantic-release/npm',                       //used if you are releasing an npm package
+        ['@semantic-release/github', {                 // uses for creating a Github release
+            assets: [                                  // Sometimes the release has some assets and options
+                { path: "build.zip", label: "build"},  // when we run 'npx semantic-release' it tries to find these two following files and upload them as assets
+                { path: "coverage.zip", label: "coverage"}
+            ]
+       
+        }]
+    ]
 
 } 
