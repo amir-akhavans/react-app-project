a1a0fde8191bd4b06e04be00f9f4e779bcf2b8de
Amir Akhavan <amir.akhavans@gmail.com>
2022-05-01 11:08:47 +0300

    index.js edited

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -4,17 +4,25 @@ const github = require('@actions/github');
 try {
 
 //  throw( new Error("Some error message"));               // Simulate some errors
+    core.debug('Debug Message');                           // Runs only if debuging is enabled 
+    core.warning('Warning Message'); 
+    core.error('Error Message'); 
 
     const name = core.getInput('who-to-greet');
+    core.setSecret('name');                                // Anything typed inside () won't be visible and ***** is shown instead
     console.log('Hello ${name}');                          // Loging Input
 
     const time = new Date();
     core.setOutput("time", time.toTimeString());           // Setting Output
 
+    core.startGroup('Logging Github Object');
     console.log(JSON.stringify(github, null, '\t'));
+    core.endGroup();
 
-} catch (error) {
+    core.exportVariable("HELLO", "hello");  // Set Environment Variables   ---->  HELLO: hello
 
-    core.setFailed('error.message')
+}   catch (error) {
+
+    core.setFailed('error.message');
 
 }
